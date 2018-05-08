a547c3f0637d3df97955ad384ea2b3cad17285db
Luke Parker <lukep@digispend.io>
2018-05-08 17:31:27 -0500

    Updated CMC

--- src/lib/CoinMarketCapEntry.java
+++ src/lib/CoinMarketCapEntry.java
@@ -3,40 +3,38 @@ package src.lib;
 import src.lib.HTTP;
 
 public class CoinMarketCapEntry {
+    public static String upArrow = "▲";
+    public static String downArrow = "▼";
+
     private String symbol, price, percent, arrow;
+    private boolean up;
 
-    public static boolean isGreen(String data) {
-        return (data.substring(0, 1) == "▲");
-    }
+    public CoinMarketCapEntry(int id) throws Exception {
+        String coinData = HTTP.get("https://api.coinmarketcap.com/v2/ticker/?start=" + id + "&limit=1");
 
-    public static String getPriceData(int id) {
-        try {
-            String symbol, price, percent, arrow;
-            String coinData = HTTP.get("https://api.coinmarketcap.com/v2/ticker/?start=" + id + "&limit=1");
+        symbol = coinData.split("\"symbol\":")[1].split("\"")[1];
+        price = coinData.split("\"price\": ")[1].split(",")[0];
+        percent = coinData.split("\"percent_change_1h\": ")[1].split(",")[0];
 
-            symbol = coinData.split("\"symbol\":")[1].split("\"")[1];
-
-            price = coinData.split("\"price\": ")[1].split(",")[0];
-
-            percent = coinData.split("\"percent_change_1h\": ")[1].split(",")[0];
-            if (percent.contains("-")) {
-                percent = percent.substring(1, percent.length());
-                arrow = "▲";
-            } else {
-                arrow = "▼";
-            }
-
-            return arrow + symbol + " $" + price + " %" + percent;
-        } catch(Exception e) {
-            return "";
+        if (percent.contains("-")) {
+            percent = percent.substring(1, percent.length());
+            up = false;
+            arrow = downArrow;
+        } else {
+            up = true;
+            arrow = upArrow;
         }
     }
 
-    public CoinMarketCapEntry(int id) {
+    public CoinMarketCapEntry(String ticker) throws Exception {
 
     }
 
-    public CoinMarketCapEntry(String ticker) {
+    public boolean isUp() {
+        return up;
+    }
 
+    public String toString() {
+        return symbol + " $" + price + " %" + percent;
     }
 }
