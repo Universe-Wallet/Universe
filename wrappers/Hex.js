281d080a9709cc306b076a92c43a197b751033d3
Luke Parker <lukep@digispend.io>
2018-05-11 11:42:06 -0500

    Simplified littleEndian

--- wrappers/Hex.js
+++ wrappers/Hex.js
@@ -1,17 +1,10 @@
 var Hex = {
     littleEndian: function(hexStr) {
         var res = "";
-        for (var i = 0; i < hexStr.length; i = i + 2) {
-            res += hexStr.substr(i+1, 1);
+        for (var i = hexStr.length-2; i >= 0; i = i - 2) {
             res += hexStr.substr(i, 1);
+            res += hexStr.substr(i+1, 1);
         }
-
-        hexStr = res;
-        res = "";
-        for (var i = 0; i < hexStr.length; i++) {
-            res += hexStr.substr(hexStr.length - (i+1), 1);
-        }
-
         return res;
     }
 }
